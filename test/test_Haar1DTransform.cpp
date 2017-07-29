089fd276f1f8a350eb739e7ff4e423a5b31e2ae6
John Aguayo <jose.john.aguayo@gmail.com>
2017-07-28 22:16:34 -0700

    WIP: Changing structure to use CMake and Google Tests
    Todo: add test cases for analysis, add synthesis functionality along with test cases
    
    this will be done in the next couple of days.

---
+++ test/test_Haar1DTransform.cpp
@@ -0,0 +1,26 @@
+#include "Haar1DTransform.hpp"
+
+#include "gtest/gtest.h"
+
+TEST(Haar1DTransform,constructor) {
+  WaveletTransform* dwt = new Haar1DTransform();
+  bool exceptionThrown;
+  double* inputData = new double[8];
+  vector<int> lengthOfDimensions;
+  double* coefficients;
+  int maxLevel = 2;
+
+  exceptionThrown = false;
+
+  inputData[0] = 4;
+  inputData[1] = 6;
+  inputData[2] = 10;
+  inputData[3] = 12;
+  inputData[4] = 8;
+  inputData[5] = 6;
+  inputData[6] = 5;
+  inputData[7] = 5;
+
+  lengthOfDimensions.push_back(8);
+  delete [] inputData;
+}
